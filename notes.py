0490c9a62d5203a1228ac3876d466aaf166b0ea2
Robert Berg <rberg@cps.edu>
2024-09-09 13:12:03 -0500

    first commit

---
+++ notes.py
@@ -0,0 +1,7 @@
+# This File will be used as notes for the basics of python
+# There is a lot in this file, you should add, subtract, change things as needed to help fully understand the process.
+# Mr. Berg will be going over all parts, but will not be grading anything in this file
+
+
+
+
